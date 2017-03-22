7d6d0132608d148944c1b3d3bd73a63a68d5aeea
Marissa <marissacomstock@gmail.com>
2017-03-22 14:58:15 -0700

    updated header

--- builderGUI/__init__.py
+++ builderGUI/__init__.py
@@ -1,10 +1,1 @@
 #!/usr/bin/env python
-# ---------------------------------------------------------------------------------------------
-"""
-
-The 'tip.maya' module package contains Maya specific modules for cross department use at Tippett Studio.
-
-"""
-# ---------------------------------------------------------------------------------------------
-
-from . import *
